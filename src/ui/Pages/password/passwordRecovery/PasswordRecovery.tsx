7b145d2fad714589d98366311854e266c304a51e
admin <1111>
2022-08-05 22:27:01 +0300

    'added validation ForgotPassword, PasswordRecovery'

--- src/ui/Pages/password/passwordRecovery/PasswordRecovery.tsx
+++ src/ui/Pages/password/passwordRecovery/PasswordRecovery.tsx
@@ -1,12 +1,17 @@
 import React, { ChangeEvent } from 'react';
 
+import { yupResolver } from '@hookform/resolvers/yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import * as Yup from 'yup';
 
+import { selectLoading } from '../../../../bll/selectors/selectors';
 import { setNewPasswordTC } from '../../../../bll/slices/passwordReducer';
 import { useAppDispatch } from '../../../../bll/store';
 import Button from '../../../components/button/Button';
 import { Input } from '../../../components/input/Input';
+import Preloader from '../../../components/preloader/Preloader';
 import { Title } from '../../../components/title/Title';
 import { AuthWrapper } from '../../../styles/authWrapper/AuthWrapper';
 
@@ -19,12 +24,30 @@ export type FormData = {
 const PasswordRecovery = () => {
   const dispatch = useAppDispatch();
   const { token } = useParams<{ token: string }>();
+  const loading = useSelector(selectLoading);
+  const formSchema = Yup.object().shape({
+    password: Yup.string()
+      .required('password is required')
+      .min(3, 'password must be at 3 char long'),
+  });
 
-  const { register, handleSubmit, reset } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({
+    mode: 'all',
+    resolver: yupResolver(formSchema),
+  });
   const onSubmit: SubmitHandler<FormData> = password => {
     dispatch(setNewPasswordTC({ password: password.password, token }));
     reset();
   };
+
+  if (loading) {
+    return <Preloader />;
+  }
 
   return (
     <AuthWrapper>
@@ -32,15 +55,19 @@ const PasswordRecovery = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={s.input}>
           <Input
-            type="password"
-            register={register}
-            label="New Password"
             name="password"
+            register={register}
+            type="password"
+            label="Password"
+            required
+            error={errors.password?.message}
           />
-          <p className={s.description}>
-            Create new password and we will send you further instructions to email
-          </p>
+          <div className={s.error}>{errors.password && errors.password.message}</div>
         </div>
+        <p className={s.description}>
+          Create new password and we will send you further instructions to email
+        </p>
+
         <div className={s.buttonContainer}>
           <Button variant="auth" name=" Create new password" type="submit" />
         </div>
