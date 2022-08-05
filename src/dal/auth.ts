d1e36fb60153f0354e6dc9cc49d4c8aff09d673b
SashaPolyanski <80836080+SashaPolyanski@users.noreply.github.com>
2022-08-05 14:30:06 +0300

    fix layout and logic

--- src/dal/auth.ts
+++ src/dal/auth.ts
@@ -25,7 +25,7 @@ export const auth = {
       from: 'test-front-admin <ai73a@yandex.by>',
       message: `<div style="background-color: lime; padding: 15px">
       password recovery link: 
-      <a href='http://localhost:3000/#/set-new-password/$token$'>
+      <a href='http://localhost:3000/#/new-password/$token$'>
       link</a>
       </div>`,
     });
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-// eslint-disable-next-line import/order
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 import { store } from './bll/store';
@@ -13,8 +11,8 @@ import { store } from './bll/store';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 );
