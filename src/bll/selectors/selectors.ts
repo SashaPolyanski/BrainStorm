743b8ab97cd423f3c01132828983ff504a361dc3
Yura Kholoimov <yurakholoimov@ya.ru>
2022-08-11 20:50:17 +0300

    Добавил обработку ошибки страницы Login, типизация ошибки через условие, создал snakBar

--- src/bll/selectors/selectors.ts
+++ src/bll/selectors/selectors.ts
@@ -1,6 +1,7 @@
 import { AppRootStateType } from '../store';
 
 export const selectIsAuth = (state: AppRootStateType) => state.auth.isLogin;
+export const authError = (state: AppRootStateType) => state.auth.error;
 export const selectLoading = (state: AppRootStateType) => state.app.isLoading;
 export const selectEmail = (state: AppRootStateType) => state.restorePassword.email;
 export const selectIsSend = (state: AppRootStateType) => state.restorePassword.isSend;
